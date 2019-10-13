67cd2402dd4ad128902b385d8a23c97585cb2f65
wc <2272311953@qq.com>
2019-10-13 19:33:54 +0800

    7777

--- src/main/java/com/example/demo/controller/GoodsController.java
+++ src/main/java/com/example/demo/controller/GoodsController.java
@@ -1,41 +1,78 @@
 package com.example.demo.controller;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import javax.annotation.Resource;
 
+import org.springframework.data.redis.core.BoundListOperations;
+import org.springframework.data.redis.core.BoundZSetOperations;
 import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.bean.Goods;
 import com.example.demo.service.GoodsService;
+import com.example.demo.utils.PageUtils;
 
 @Controller
 public class GoodsController {
 	@Resource
-	private GoodsService service; 
+	private GoodsService goodsService;
+
 	@Resource
 	private RedisTemplate<String, Object> redisTemplate;
+
 	@RequestMapping("list")
-	public Object goodsList(Model model) {
-		List<Goods>list=service.getgoodsList();
-		model.addAttribute("list", list);
+	public String list(Model model ,
+			@RequestParam(defaultValue="1")long cpage,
+			@RequestParam(defaultValue="")String  orders
+			) {
+		
+		/*List<Goods> goodsList = goodsService.getGoodsList();*/
+		////RedisTemplate操作list集合的第二种方式
+		BoundListOperations<String,Object> boundListOps = redisTemplate.boundListOps("goodsList");
+		Map<String, Long> pageMap = PageUtils.pageUtils(boundListOps.size(), cpage);
+
+		List<Object> range = boundListOps.range(pageMap.get("start"),pageMap.get("end"));
+		
+		//装载未排序的list集合
+		model.addAttribute("goodsList", range);
+		//如果需要排序的话将查询出来的list集合装入zset中按照售出的百分比进行排序
+		if(orders.length()>0) {
+			BoundZSetOperations<String,Object> boundZSetOps = redisTemplate.boundZSetOps("sortGoods");
+			for (Object object : range) {
+				Goods goods = (Goods) object;
+				boundZSetOps.add(goods, goods.getSaleCount()/goods.getGcount());
+			}
+			Set<Object> range2 = boundZSetOps.range(0, -1);
+			model.addAttribute("goodsList", range2);
+		}
+		
+		model.addAttribute("pages", pageMap.get("pages"));
+		model.addAttribute("cpage", pageMap.get("cpage"));
+		
+		
 		return "list";
 	}
-	@RequestMapping("toadd")
-	public Object toadd(Model model) {
-		
+
+	
+
+	@RequestMapping("toAdd")
+	public String toAdd() {
 		return "add";
 	}
-	@RequestMapping("addgoods")
-	public Object addgoods(Goods goods) {
+	@RequestMapping("addGoods")
+	public String addGoods(Goods goods) {
+		//RedisTemplate操作list集合的第一种方式
 		ListOperations<String, Object> opsForList = redisTemplate.opsForList();
-		Long leftPush = opsForList.leftPush("goodslist", goods);
-		System.err.println(leftPush);
-		
-		return "redirect:list";
+
+		Long push = opsForList.leftPush("goodsList", goods);
+		System.out.println(push);
+		return "add";
 	}
 }
--- src/main/webapp/WEB-INF/jsp/list.jsp
+++ src/main/webapp/WEB-INF/jsp/list.jsp
@@ -13,24 +13,38 @@
 <script type="text/javascript" src="js/jquery-1.8.2.min.js">
 </script>
 <body>
-<a href="toadd">添加</a>
+<a href="toAdd">添加</a>
+<a href="list?orders=desc&cpage=${cpage }">售出比例降序排序</a>
+
 <table>
 	<tr>
 		<td>编号</td>
-		<td>商品</td>
+		<td>商品名称</td>
 		<td>价格</td>
 		<td>总量</td>
 		<td>售出</td>
+		<td>售出比例</td>
 	</tr>
-	<c:forEach items="${list }" var="goods">
-			<tr>
-		<td>${goods.gid }</td>
+	<c:forEach items="${goodsList }" var="goods" varStatus="var">
+	<tr>
+		<td>${var.count}</td>
 		<td>${goods.gname }</td>
 		<td>${goods.price }</td>
 		<td>${goods.gcount }</td>
 		<td>${goods.saleCount }</td>
+		<td>
+			<fmt:formatNumber pattern="##.00" value="${goods.saleCount/goods.gcount*100 }"></fmt:formatNumber>%</td>
 	</tr>
 	</c:forEach>
+
+	<tr>
+		<td colspan="5">
+			<a href="list?cpage=1">首页</a>
+			<a href="list?cpage=${cpage-1 }">上一页</a>
+			<a href="list?cpage=${cpage+1 }">下一页</a>
+			<a href="list?cpage=${pages }">尾页</a>
+		</td>
+	</tr>
 </table>
 </body>
 </html>
